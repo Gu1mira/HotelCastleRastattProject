b776a29da077efb37388663508bd657a189eb984
gu1mira <sidagulova@gmail.com>
2025-05-21 09:34:38 +0300

    Data Table, DialogContent, Headers and Parent pages are added

---
+++ src/test/java/pages/DialogContent.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.GWD;
+import utilities.ReusableMethods;
+
+import java.util.List;
+
+public class DialogContent extends ReusableMethods {
+    public DialogContent() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
+
+
+}
