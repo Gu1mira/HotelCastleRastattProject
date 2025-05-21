6cff7b4b7faa9d4dae4fb05388fd6e1a09d56cc5
gu1mira <sidagulova@gmail.com>
2025-05-21 12:34:22 +0300

    DialogContent and Headers are updated

--- src/test/java/pages/DialogContent.java
+++ src/test/java/pages/DialogContent.java
@@ -13,5 +13,19 @@ public class DialogContent extends ReusableMethods {
         PageFactory.initElements(GWD.getDriver(), this);
     }
 
+    //US_08_Book_Now
+    @FindBy(xpath = "(//a[text()='JETZT BUCHEN'])[3]")
+    public WebElement jetztBuchenButton;
+
+    public WebElement getWebElement(String strElementName) {
+
+        switch (strElementName.trim()) {
+            case "Jetzt Buchen":
+                return this.jetztBuchenButton;
+
+        }
+        return null;
+    }
+
 
 }
--- src/test/java/pages/Headers.java
+++ src/test/java/pages/Headers.java
@@ -1,4 +1,44 @@
 package pages;
 
-public class Headers {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.GWD;
+import utilities.ReusableMethods;
+
+public class Headers extends ReusableMethods {
+    public Headers() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
+
+    //US_01_Homepage Logo
+
+    @FindBy(xpath = "//*[text()='Start']")
+    public WebElement start;
+
+    @FindBy(xpath = "//*[text()='Über uns']")
+    public WebElement uberUns;
+
+    @FindBy(xpath = "//*[text()='Anfahrt']")
+    public WebElement anfahrt;
+
+    @FindBy(xpath = "//*[text()='Aktivitäten']")
+    public WebElement aktivitäten;
+
+    //US_08_Book_Now
+    @FindBy(xpath = "(//a[text()='JETZT BUCHEN'])[3]")
+    public WebElement jetztBuchenButton;
+
+
+    public WebElement getWebElement(String strElementName) {
+
+        switch (strElementName.trim()) {
+            case "Jetzt Buchen":
+                return this.jetztBuchenButton;
+
+        }
+        return null;
+    }
+
+
 }
