4286777a987be6ef2812e745c47e7ecf58902e7b
elcnburak <elcnburak@hotmail.com>
2025-05-28 21:32:16 +0100

    _06_ and _11_Step created.

---
+++ src/test/java/stepDefinitions/_06_Location.java
@@ -0,0 +1,30 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+
+public class _06_Location {
+    @When("The user clicks on the Anfahrt tab from the top menu")
+    public void theUserClicksOnTheAnfahrtTabFromTheTopMenu() {
+    }
+
+    @And("The user should see German language on the page")
+    public void theUserShouldSeeGermanLanguageOnThePage() {
+    }
+
+    @And("The user clicks on the English language option icon in the top right corner of the header menu")
+    public void theUserClicksOnTheEnglishLanguageOptionIconInTheTopRightCornerOfTheHeaderMenu() {
+    }
+
+    @And("The user should see English language on the page. Keeping the same structure.")
+    public void theUserShouldSeeEnglishLanguageOnThePageKeepingTheSameStructure() {
+    }
+
+    @And("The user should see {string} and the explanatory text below it")
+    public void theUserShouldSeeAndTheExplanatoryTextBelowIt(String arg0) {
+    }
+
+    @And("The user should see Google Maps correctly")
+    public void theUserShouldSeeGoogleMapsCorrectly() {
+    }
+}
