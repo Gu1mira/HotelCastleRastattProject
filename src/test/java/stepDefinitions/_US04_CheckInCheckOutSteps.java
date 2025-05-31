8046093505b727a9384f13b215054a2d118e36cb
elcnburak <elcnburak@hotmail.com>
2025-05-31 22:54:13 +0100

    _06_Location completed.

--- src/test/java/stepDefinitions/_US04_CheckInCheckOutSteps.java
+++ src/test/java/stepDefinitions/_US04_CheckInCheckOutSteps.java
@@ -16,69 +16,58 @@ import java.util.List;
 import java.util.Locale;
 
 public class _US04_CheckInCheckOutSteps {
-    DialogContent dc = new DialogContent();
+
     LocalDate today = LocalDate.now();
     YearMonth currentMonth = YearMonth.now();
     int dayOfMonth = today.getDayOfMonth();
-    String dayOfMont = String.valueOf(dayOfMonth);
     String year = String.valueOf(today.getYear());
     String month = String.valueOf(today.getMonth());
-    List<Integer> checkInPreviousDay = new ArrayList<>();
-    List<String> previousDayStr = new ArrayList<>();
     int lastDayOfMonth = currentMonth.lengthOfMonth();
+
     int randomCheckInDate = (int) (Math.random() * (lastDayOfMonth - dayOfMonth + 1)) + dayOfMonth;
-    List<Integer> checkOutPreviousDays = new ArrayList<>();
-    int randomCheckOutDate = (int) (Math.random() * (lastDayOfMonth - randomCheckInDate)) + randomCheckInDate+1;
-    String randomCheckInDateStr = String.valueOf(randomCheckInDate);
-    String randomCheckOutDateStr = String.valueOf(randomCheckOutDate);
-    String checkInDate = randomCheckInDateStr + "/" + String.format("%02d", today.getMonthValue()) + "/" + year;
-    String checkOutDate = randomCheckOutDateStr + "/" + String.format("%02d", today.getMonthValue()) + "/" + year;
+    int randomCheckOutDate = (int) (Math.random() * (lastDayOfMonth - randomCheckInDate)) + randomCheckInDate + 1;
+
+    String checkInDate = randomCheckInDate + "/" + String.format("%02d", today.getMonthValue()) + "/" + year;
+    String checkOutDate = randomCheckOutDate + "/" + String.format("%02d", today.getMonthValue()) + "/" + year;
+
     LocalDate nextMonthDate = today.plusMonths(1);
     String nextMonthName = nextMonthDate.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
 
     @Given("Navigate to the Hotel Castle Rastatt")
     public void navigateToTheHotelCastleRastatt() {
         GWD.getDriver().get(ConfigReader.getProperty("URL"));
-        dc.clickFunction(dc.acceptCookies);
-
+        new DialogContent().clickFunction(new DialogContent().acceptCookies);
     }
 
     @When("The customer click  on the english language icon")
     public void theCustomerClickOnTheEnglishLanguageIcon() {
-        dc.clickFunction(dc.englishIcon);
-
+        new DialogContent().clickFunction(new DialogContent().englishIcon);
     }
 
     @And("The customer clicks on the check-in calender icon")
     public void theCustomerClicksOnTheCheckInCalenderIcon() {
-        dc.clickFunction(dc.checkInWidget);
-
+        new DialogContent().clickFunction(new DialogContent().checkInWidget);
     }
 
     @Then("Verifies that today's date, month and previous days cannot be selected")
     public void verifiesThatTodaySDateMonthAndPreviousDaysCannotBeSelected() {
-        dayOfMonth = today.getDayOfMonth();
-        dayOfMont = String.valueOf(dayOfMonth);
-        year = String.valueOf(today.getYear());
-        month = String.valueOf(today.getMonth());
-        checkInPreviousDay = new ArrayList<>();
-        previousDayStr = new ArrayList<>();
+        DialogContent dc = new DialogContent();
+        List<Integer> checkInPreviousDay = new ArrayList<>();
 
         for (int i = dayOfMonth; i > 0; i--) {
             checkInPreviousDay.add(i);
         }
 
-
         for (int i = 0; i < dc.checkIndateList.size(); i++) {
-
             if (checkInPreviousDay.get(i) < dayOfMonth) {
                 dc.wait.until(ExpectedConditions.visibilityOf(dc.checkIndateList.get(i)));
                 Assert.assertEquals(dc.checkIndateList.get(i).getAttribute("class"), "is-disabled");
                 break;
             }
         }
+
         for (WebElement day : dc.checkIndateList) {
-            if (day.getText().equals(dayOfMont)) {
+            if (day.getText().equals(String.valueOf(dayOfMonth))) {
                 dc.wait.until(ExpectedConditions.visibilityOf(day));
                 Assert.assertEquals(day.getAttribute("class"), "is-today is-selected");
                 break;
@@ -103,34 +92,31 @@ public class _US04_CheckInCheckOutSteps {
 
     @And("The customer enters  the check-in date")
     public void theCustomerEntersTheCheckInDate() {
-        System.out.println(randomCheckInDate);
+        DialogContent dc = new DialogContent();
         dc.clickFunction(dc.checkIndateList.get(randomCheckInDate - 1));
     }
 
     @And("The customer clicks on the check-out calender icon")
     public void theCustomerClicksOnTheCheckOutCalenderIcon() {
-        dc.clickFunction(dc.checkOutWidget);
-
-
+        new DialogContent().clickFunction(new DialogContent().checkOutWidget);
     }
 
     @Then("Verifies that today's date, month, previous days and entry date cannot be selected")
     public void verifiesThatTodaySDateMonthPreviousDaysAndEntryDateCannotBeSelected() {
-
+        DialogContent dc = new DialogContent();
+        List<Integer> checkOutPreviousDays = new ArrayList<>();
         for (int i = randomCheckInDate; i > 0; i--) {
             checkOutPreviousDays.add(i);
         }
 
-        if ((randomCheckInDate) == (lastDayOfMonth)) {
+        if (randomCheckInDate == lastDayOfMonth) {
             for (WebElement Month : dc.checkOutMonthNames) {
                 if (Month.getText().equals(nextMonthName)) {
                     Assert.assertTrue(Month.isSelected());
                 }
             }
         } else {
-
             for (int i = 0; i < dc.checkOutdateList.size(); i++) {
-
                 if (checkOutPreviousDays.get(i) < randomCheckInDate) {
                     dc.wait.until(ExpectedConditions.visibilityOf(dc.checkOutdateList.get(i)));
                     Assert.assertEquals(dc.checkOutdateList.get(i).getAttribute("class"), "is-disabled");
@@ -145,31 +131,24 @@ public class _US04_CheckInCheckOutSteps {
                 }
             }
         }
-
     }
 
     @And("The customer enters  the check-out date")
     public void theCustomerEntersTheCheckOutDate() {
-        if (dc.checkIndateList.get(randomCheckInDate - 1).getText().equals(String.valueOf(lastDayOfMonth))) {
-            System.out.println(dc.checkIndateList.get(randomCheckInDate - 1).getText());
-            dc.clickFunction(dc.checkOutdateList.get((int) (Math.random() * (lastDayOfMonth)) + 1));
+        DialogContent dc = new DialogContent();
 
+        if (dc.checkIndateList.get(randomCheckInDate - 1).getText().equals(String.valueOf(lastDayOfMonth))) {
+            dc.clickFunction(dc.checkOutdateList.get((int) (Math.random() * (lastDayOfMonth)) + 1));
         } else {
-            System.out.println(randomCheckOutDate);
             dc.clickFunction(dc.checkOutdateList.get(randomCheckOutDate - 1));
         }
-
     }
 
     @Then("Verifies check-in and check-out dates")
     public void verifiesCheckInAndCheckOutDates() {
-        dc.wait.until(ExpectedConditions.attributeContains(dc.checkInWidget, "value", "2025"));
-        dc.wait.until(ExpectedConditions.attributeContains(dc.checkOutWidget, "value", "2025"));
-        System.out.println(dc.checkInWidget.getAttribute("value"));
-        System.out.println(dc.checkOutWidget.getAttribute("value"));
-        System.out.println(checkInDate);
-        System.out.println(checkOutDate);
-
+        DialogContent dc = new DialogContent();
+        dc.wait.until(ExpectedConditions.attributeContains(dc.checkInWidget, "value", year));
+        dc.wait.until(ExpectedConditions.attributeContains(dc.checkOutWidget, "value", year));
         dc.wait.until(ExpectedConditions.attributeToBe(dc.checkInWidget, "value", checkInDate));
         Assert.assertEquals(dc.checkInWidget.getAttribute("value"), checkInDate);
         dc.wait.until(ExpectedConditions.attributeToBe(dc.checkOutWidget, "value", checkOutDate));
@@ -178,23 +157,22 @@ public class _US04_CheckInCheckOutSteps {
 
     @When("The customer clicks the GO button")
     public void theCustomerClicksTheGoButton() {
-        dc.clickFunction(dc.goButton);
+        new DialogContent().clickFunction(new DialogContent().goButton);
     }
 
     @Then("Verify redirection to the reservation page")
     public void verifyRedirectionToTheReservationPage() {
+        DialogContent dc = new DialogContent();
         dc.switchToWindow(1);
         String noReservation = "Es tut uns leid, aber für das angegebene Datum sind keine Unterkünfte verfügbar!";
         dc.wait.until(ExpectedConditions.urlContains("reservation"));
         dc.verifyContainsText(dc.hotelCastleRastattText, "Rastatt");
-        System.out.println(dc.hotelCastleRastattText.getText());
         dc.wait.until(ExpectedConditions.attributeToBe(dc.spinnerContainer, "style", "display: none;"));
+
         if (!dc.messageContainer.isEmpty()) {
-            Assert.assertTrue(dc.messageContainer.get(0).getText().contains(noReservation), "Expected message not found");
-            System.out.println(dc.messageContainer.get(0).getText());
+            Assert.assertTrue(dc.messageContainer.get(0).getText().contains(noReservation));
         } else if (!dc.availableRoom.isEmpty()) {
             Assert.assertTrue(dc.availableRoom.get(0).getText().contains("übrig"));
-            System.out.println(dc.availableRoom.get(0).getText());
         }
     }
 }
