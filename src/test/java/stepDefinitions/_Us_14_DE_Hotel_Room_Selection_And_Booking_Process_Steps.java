48e388bbf032c932535197c23be33c7b94f120e9
Degisiklikler1 <megaozi9517536@gmail.com>
2025-07-20 21:42:02 +0300

    _Us_14_DE_Hotel_Room_Selection_And_Booking_Process_Steps is created

---
+++ src/test/java/stepDefinitions/_Us_14_DE_Hotel_Room_Selection_And_Booking_Process_Steps.java
@@ -0,0 +1,31 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _Us_14_DE_Hotel_Room_Selection_And_Booking_Process_Steps {
+    @When("The customer click JetztBuchen button")
+    public void theCustomerClickJetztBuchenButton() {
+    }
+
+    @And("The customer enters checkin and checkout dates")
+    public void theCustomerEntersCheckinAndCheckoutDates() {
+    }
+
+    @And("The customer click suchen button")
+    public void theCustomerClickSuchenButton() {
+    }
+
+    @When("The customer chooses the number of adults,chilren,and rooms")
+    public void theCustomerChoosesTheNumberOfAdultsChilrenAndRooms() {
+    }
+
+    @Then("The Customer verifies adult and child choices with information on the maximum number of rooms")
+    public void theCustomerVerifiesAdultAndChildChoicesWithInformationOnTheMaximumNumberOfRooms() {
+    }
+
+    @Then("The Customer chooses the number of rooms and verify")
+    public void theCustomerChoosesTheNumberOfRoomsAndVerify() {
+    }
+}
