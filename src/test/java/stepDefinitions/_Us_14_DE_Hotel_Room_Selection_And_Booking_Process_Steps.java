ffd707734a7bd27b7be60b9f2604b2aa81f7467d
Degisiklikler1 <megaozi9517536@gmail.com>
2025-07-20 21:43:17 +0300

    _Us_14_DE_Hotel_Room_Selection_And_Booking_Process_Steps is Updated

--- src/test/java/stepDefinitions/_Us_14_DE_Hotel_Room_Selection_And_Booking_Process_Steps.java
+++ src/test/java/stepDefinitions/_Us_14_DE_Hotel_Room_Selection_And_Booking_Process_Steps.java
@@ -38,6 +38,28 @@ public class _Us_14_DE_Hotel_Room_Selection_And_Booking_Process_Steps {
 
     @When("The customer chooses the number of adults,chilren,and rooms")
     public void theCustomerChoosesTheNumberOfAdultsChilrenAndRooms() {
+
+        dc.wait(3);
+        randomRoom = dc.randomGenerator(dc.maximaleBelegungList.size());
+
+        dc.clickFunction(dc.ErwachseneList.get(randomRoom));
+        dc.clickFunction(dc.ErwachseneSelect.get(dc.randomGenerator(dc.ErwachseneSelect.size())));
+        dc.clickFunction(dc.KinderList.get(randomRoom));
+        dc.jsClick(dc.KinderSelect.get(dc.randomGenerator(dc.KinderSelect.size())));
+
+        dc.jsClick(dc.ZimmerList.get(randomRoom));
+        if (dc.roomName.get(randomRoom).getText().equals("Einzelzimmer Standard")) {
+            int a = Integer.parseInt(dc.availableRoom.get(0).getText().replaceAll("[^0-9,.]", ""));
+            dc.clickFunction(dc.ZimmerSelect.get((int) (Math.random() * a) + 1));
+        } else if (dc.roomName.get(randomRoom).getText().equals("Doppelzimmer Standard")) {
+            int b = Integer.parseInt(dc.availableRoom.get(1).getText().replaceAll("[^0-9,.]", ""));
+            dc.clickFunction(dc.ZimmerSelect.get((int) (Math.random() * b) + 1));
+        } else if (dc.roomName.get(randomRoom).getText().equals("Doppelzimmer Komfort")) {
+            dc.clickFunction(dc.ZimmerSelect.get((int) (Math.random() * 10) + 1));
+        } else if (dc.roomName.get(randomRoom).getText().equals("Familien Suite (Dreibettzimmer)")) {
+            int c = Integer.parseInt(dc.availableRoom.get(2).getText().replaceAll("[^0-9,.]", ""));
+            dc.clickFunction(dc.ZimmerSelect.get((int) (Math.random() * c) + 1));
+        }
     }
 
     @Then("The Customer verifies adult and child choices with information on the maximum number of rooms")
