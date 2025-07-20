9bace005148b874f29beb5f4944fe9c62bd6d3fa
Degisiklikler1 <megaozi9517536@gmail.com>
2025-07-20 21:42:58 +0300

    _Us_14_DE_Hotel_Room_Selection_And_Booking_Process_Steps is Updated

--- src/test/java/stepDefinitions/_Us_14_DE_Hotel_Room_Selection_And_Booking_Process_Steps.java
+++ src/test/java/stepDefinitions/_Us_14_DE_Hotel_Room_Selection_And_Booking_Process_Steps.java
@@ -3,18 +3,37 @@ package stepDefinitions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.DialogContent;
 
 public class _Us_14_DE_Hotel_Room_Selection_And_Booking_Process_Steps {
+
+    DialogContent dc = new DialogContent();
+
+    String checkIndate = "20092025";
+    int checkInDay = Integer.parseInt(checkIndate.substring(0, 2));
+    String checkOutDate = "27092025";
+    int checkOutDay = Integer.parseInt(checkOutDate.substring(0, 2));
+    int holidayDayNumber = (checkOutDay - checkInDay);
+    int randomRoom;
+
     @When("The customer click JetztBuchen button")
     public void theCustomerClickJetztBuchenButton() {
+        dc.clickFunction(dc.jetztBuchenButton);
     }
 
     @And("The customer enters checkin and checkout dates")
     public void theCustomerEntersCheckinAndCheckoutDates() {
+
+        dc.clickFunction(dc.checkInPLc);
+        dc.pressCtrlA();
+        dc.checkInPLc.sendKeys(checkIndate);
+        dc.tabKeyMultiplePress(1);
+        dc.checkOutPLc.sendKeys(checkOutDate);
     }
 
     @And("The customer click suchen button")
     public void theCustomerClickSuchenButton() {
+        dc.clickFunction(dc.suchenButton);
     }
 
     @When("The customer chooses the number of adults,chilren,and rooms")
